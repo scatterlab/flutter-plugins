d35e121c0b9c976bc48985728b641a7c3e11c273
kattergil <gil@beomjun.kr>
2020-12-17 16:25:58 +0900

    Add `SKPaymentDiscountWrapper` to `store_kit_wrappers`

--- packages/in_app_purchase/lib/src/store_kit_wrappers/sk_payment_queue_wrapper.g.dart
+++ packages/in_app_purchase/lib/src/store_kit_wrappers/sk_payment_queue_wrapper.g.dart
@@ -22,18 +22,42 @@ Map<String, dynamic> _$SKErrorToJson(SKError instance) => <String, dynamic>{
       'userInfo': instance.userInfo,
     };
 
+SKPaymentDiscountWrapper _$SKPaymentDiscountWrapperFromJson(Map json) {
+  return SKPaymentDiscountWrapper(
+    identifier: json['identifier'] as String,
+    keyIdentifier: json['keyIdentifier'] as String,
+    nonce: json['nonce'] as String,
+    signature: json['signature'] as String,
+    timestamp: json['timestamp'] as int,
+  );
+}
+
+Map<String, dynamic> _$SKPaymentDiscountWrapperToJson(
+        SKPaymentDiscountWrapper instance) =>
+    <String, dynamic>{
+      'identifier': instance.identifier,
+      'keyIdentifier': instance.keyIdentifier,
+      'nonce': instance.nonce,
+      'signature': instance.signature,
+      'timestamp': instance.timestamp,
+    };
+
 SKPaymentWrapper _$SKPaymentWrapperFromJson(Map json) {
   return SKPaymentWrapper(
     productIdentifier: json['productIdentifier'] as String,
     applicationUsername: json['applicationUsername'] as String,
     requestData: json['requestData'] as String,
     quantity: json['quantity'] as int,
+    paymentDiscount: json['paymentDiscount'] == null
+        ? null
+        : SKPaymentDiscountWrapper.fromJson(json['paymentDiscount'] as Map),
     simulatesAskToBuyInSandbox: json['simulatesAskToBuyInSandbox'] as bool,
   );
 }
 
 Map<String, dynamic> _$SKPaymentWrapperToJson(SKPaymentWrapper instance) =>
     <String, dynamic>{
+      'paymentDiscount': instance.paymentDiscount,
       'productIdentifier': instance.productIdentifier,
       'applicationUsername': instance.applicationUsername,
       'requestData': instance.requestData,
